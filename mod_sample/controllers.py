75105169ffdca77d878953c738aa707ad738889c
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2016-07-17 02:23:52 +0200

    Move media parser to own class
    Media info parser was a bit too big to keep in the controller, thus move
    it to it's own class.

--- mod_sample/controllers.py
+++ mod_sample/controllers.py
@@ -1,11 +1,13 @@
 import os
 
-import xmltodict
 from flask import Blueprint, make_response
 
 from decorators import template_renderer
 from mod_auth.controllers import login_required, check_access_rights
 from mod_auth.models import Role
+from mod_sample.media_info_parser import MediaInfoFetcher, \
+    InvalidMediaInfoError
+
 from mod_sample.models import Sample, ExtraFile, ForbiddenExtension
 
 mod_sample = Blueprint('sample', __name__)
@@ -17,111 +19,20 @@ class SampleNotFoundException(Exception):
         self.message = message
 
 
-def fetch_media_info(sample):
-    from run import config, log
-    # Fetch media info
-    media_info_path = os.path.join(
-        config.get('SAMPLE_REPOSITORY', ''), 'TestFiles', 'media',
-        sample.sha + '.xml')
-    if os.path.isfile(media_info_path):
-        result = []
-        with open(media_info_path) as fd:
-            doc = xmltodict.parse(fd.read())
-            if 'Mediainfo' in doc:
-                media_info = doc['Mediainfo']
-                result.append({
-                    'name': 'Media info version',
-                    'value': media_info['@version']
-                })
-                file_info = media_info['File']
-                if 'track' in file_info:
-                    tracks_processed = 0
-                    captions = []
-                    for track in file_info['track']:
-                        if track['@type'] == 'General':
-                            # Process general info
-                            value = {
-                                'Format': track['Format'],
-                                'File size': track['File_size']
-                            }
-                            if 'Duration' in track:
-                                value['Duration'] = track['Duration']
-                            if 'Codec_ID' in track:
-                                value['Codec id'] = track['Codec_ID']
-                        elif track['@type'] == 'Video':
-                            # Process video info
-                            v_format = track['Format']
-                            if 'Format_Info' in track:
-                                v_format += ' ({info})'.format(
-                                    info=track['Format_Info'])
-                            v_rate = track['Frame_rate']
-                            if 'Frame_rate_mode' in track:
-                                v_rate += ' (mode: {mode})'.format(
-                                    mode=track['Frame_rate_mode'])
-                            v_scan = track['Scan_type']
-                            if 'Scan_order' in track:
-                                v_scan += ' ({order})'.format(
-                                    order=track['Scan_order'])
-                            value = {
-                                'Format': v_format,
-                                'Resolution': '{width} x {height}'.format(
-                                    width=track['Width'],
-                                    height=track['Height']),
-                                'Ratio': track['Display_aspect_ratio'],
-                                'Frame rate': v_rate,
-                                'Scan type': v_scan
-                            }
-                            if 'Duration' in track:
-                                value['Duration'] = track['Duration']
-                            if 'Writing_library' in track:
-                                value['Writing library'] = \
-                                    track['Writing_library']
-                            if 'Codec_ID' in track:
-                                value['Codec id'] = track['Codec_ID']
-                        elif track['@type'] == 'Text':
-                            # Process caption/subtitle info
-                            value = {
-                                'Format': track['Format'],
-                                'Muxing mode': track['Muxing_mode']
-                            }
-                            if 'Menu_ID' in track:
-                                value['Menu Id'] = track['Menu_ID']
-                            captions.append({
-                                'name': 'ID %s' % track['ID'],
-                                'value': value
-                            })
-                            continue
-                        else:
-                            log.debug('Unknown track type: %s' %
-                                      track['@type'])
-                            continue
-                        result.append({
-                            'name': track['@type'],
-                            'value': value
-                        })
-                        tracks_processed += 1
-                    result.append({
-                        'name': 'Captions/Subtitles',
-                        'value': captions
-                    })
-                else:
-                    result['General info'] = "Could not fetch general info"
-            else:
-                result['General'] = 'Could not load media info'
-        return result
-    return None
-
-
 def display_sample_info(sample):
-    media_info = fetch_media_info(sample)
-    if media_info is not None:
-        return {
-            'sample': sample,
-            'media': media_info,
-            'additional_files': ExtraFile.query.filter(
-                ExtraFile.sample_id == sample.id).all()
-        }
-    raise SampleNotFoundException('Could not load media info')
+    media_info_fetcher = MediaInfoFetcher(sample)
+    try:
+        media_info = media_info_fetcher.get_media_info()
+        print media_info
+    except InvalidMediaInfoError as i:
+        raise SampleNotFoundException(i.message)
+
+    return {
+        'sample': sample,
+        'media': media_info,
+        'additional_files': ExtraFile.query.filter(
+            ExtraFile.sample_id == sample.id).all()
+    }
 
 
 @mod_sample.errorhandler(SampleNotFoundException)
---
+++ mod_sample/media_info_parser.py
@@ -0,0 +1,153 @@
+import os
+import traceback
+
+import xmltodict
+
+
+class InvalidMediaInfoError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self)
+        self.message = message
+
+
+class MediaInfoFetcher:
+    def __init__(self, sample):
+        from run import config
+        # Fetch media info
+        media_info_path = os.path.join(
+            config.get('SAMPLE_REPOSITORY', ''), 'TestFiles', 'media',
+            sample.sha + '.xml')
+        if os.path.isfile(media_info_path):
+            with open(media_info_path) as fd:
+                doc = xmltodict.parse(fd.read())
+                if 'Mediainfo' in doc:
+                    self.media_info = doc['Mediainfo']
+                    self.video_tracks = []
+                    self.caption_tracks = []
+                    self.audio_tracks = []
+                    self.other_tracks = []
+                    self.general_track = {}
+                    self.parsed = False
+                else:
+                    raise InvalidMediaInfoError(
+                        'No Mediainfo root element present')
+        else:
+            raise InvalidMediaInfoError(
+                'File {path} not found'.format(path=media_info_path))
+
+    def get_media_info(self, force_parse=False):
+        result = [{
+            'name': 'Media info version',
+            'value': self.media_info['@version']
+        }]
+
+        if not self.parsed or force_parse:
+            self._process_tracks()
+
+        result.append({
+            'name': 'General',
+            'value': self.general_track
+        })
+        result.append({
+            'name': 'Video',
+            'value': self.video_tracks
+        })
+        result.append({
+            'name': 'Captions',
+            'value': self.caption_tracks
+        })
+        return result
+
+    def _process_tracks(self):
+        # Reset stored tracks
+        if self.parsed:
+            self.video_tracks = []
+            self.caption_tracks = []
+            self.audio_tracks = []
+            self.other_tracks = []
+            self.general_track = {}
+            self.parsed = False
+
+        try:
+            file_info = self.media_info['File']
+            if 'track' in file_info:
+                for track in file_info['track']:
+                    self._process_track(track)
+                # Ran through all tracks, no errors, so parsed successfully
+                self.parsed = True
+            else:
+                raise InvalidMediaInfoError('No tracks present in XML')
+        except KeyError:
+            traceback.print_exc()
+            raise InvalidMediaInfoError('No File element present in XML')
+
+    def _process_track(self, track):
+        if '@type' not in track:
+            raise InvalidMediaInfoError('Track file does not contain a type')
+        track_type = track['@type']
+        if track_type == 'General':
+            self._process_general(track)
+        elif track_type == 'Video':
+            self._process_video(track)
+        elif track_type == 'Audio':
+            # Implement at some point
+            pass
+        elif track_type == 'Text':
+            self._process_text(track)
+        # Other tracks are ignored for now
+        return
+
+    def _process_generic(self, track, keys):
+        result = {}
+        # Values we could use, but they're not always present
+        for key in keys:
+            if key in track:
+                result[key.replace('_', ' ')] = track[key]
+        return result
+
+    def _process_general(self, track):
+        self.general_track = self._process_generic(
+            track, ['Format', 'File_size', 'Duration', 'Codec_ID']
+        )
+
+    def _process_video(self, track):
+        result = self._process_generic(
+            track, ['Display_aspect_ratio', 'Writing_library', 'Duration',
+                    'Codec_ID'])
+        # Append non standard ones
+        if 'Width' in track and 'Height' in track:
+            result['Resolution'] = '{width} x {height}'.format(
+                width=track['Width'],
+                height=track['Height'])
+
+        if 'Format' in track:
+            v_format = track['Format']
+            if 'Format_Info' in track:
+                v_format += ' ({info})'.format(info=track['Format_Info'])
+            result['Format'] = v_format
+
+        if 'Frame_rate' in track:
+            v_rate = track['Frame_rate']
+            if 'Frame_rate_mode' in track:
+                v_rate += ' (mode: {mode})'.format(
+                    mode=track['Frame_rate_mode'])
+            result['Frame rate'] = v_rate
+
+        if 'Scan_type' in track:
+            v_scan = track['Scan_type']
+            if 'Scan_order' in track:
+                v_scan += ' ({order})'.format(order=track['Scan_order'])
+            result['Scan type'] = v_scan
+
+        name = 'Stream nr. {number}'.format(number=len(self.video_tracks))
+        if 'ID' in track:
+            name = 'ID: {number}'.format(number=track['ID'])
+
+        self.video_tracks.append({'name': name, 'value': result})
+
+    def _process_text(self, track):
+        self.caption_tracks.append({
+            'name': 'ID: {number}'.format(number=track['ID']),
+            'value': self._process_generic(
+                track, ['Format', 'Menu_ID', 'Muxing_mode'])
+        })

27476ed7d85b7d2dc261bdaf8362e482a9e4bb8c
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2016-09-04 17:58:05 +0200

    Add missing value

--- mod_test/controllers.py
+++ mod_test/controllers.py
@@ -42,7 +42,8 @@ def not_found(error):
 @template_renderer()
 def index():
     return {
-        'tests': Test.query.order_by(Test.id.desc()).limit(50).all()
+        'tests': Test.query.order_by(Test.id.desc()).limit(50).all(),
+        'TestType': TestType
     }
 
 

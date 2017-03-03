d4f6ba39ce359e63de22efc36d41a6dc442b7c96
Pranav Siddharth S <pranavsid98@gmail.com>
2017-03-03 13:11:38 +0530

    Update forms.py

--- mod_auth/forms.py
+++ mod_auth/forms.py
@@ -7,11 +7,11 @@ from mod_auth.models import User, Role
 
 
 def unique_username(form, field):
-    	"""
-    	Check if a user already exists with this name
+    """
+	Check if a user already exists with this name
 
 	:param field: The data value for the 'name' inserted by new User
-	:type field.data : str
+	:type field : StringField
 	"""
     user = User.query.filter(User.name == field.data).first()
     if user is not None:
@@ -23,7 +23,7 @@ def valid_password(form, field):
 	Function to check for validity of a password
 
 	:param field: The data value for the 'password' inserted by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
     if len(field.data) == 0:
         raise ValidationError('new password cannot be empty')
@@ -36,8 +36,11 @@ def valid_password(form, field):
 def email_not_in_use(has_user_field=False):
 	"""
 	Function to check if the passed email is already in use.
- 
+
+	:param has_user_field : Whether an email has an existing User (False by default)
+	:type has_user_field : boolean
 	"""
+	def _email_not_in_use(form, field):
         user_id = -1 if not has_user_field else form.user.id
         # Check if email is not already in use
         user = User.query.filter(User.email == field.data).first()
@@ -51,7 +54,7 @@ def role_id_is_valid(form, field):
 	Checks for validity of User's Role
 
 	:param field : The data value for the 'role' inserted by User
-	:type field.data : Role
+	:type field : SelectField
 	"""
     role = Role.query.filter(Role.id == field.data).first()
     if role is None:
@@ -116,7 +119,7 @@ class CompleteSignupForm(Form):
 	Validates if the repeated password is the same as 'password'
 
 	:param field : The data value for the 'password' entered by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
         if field.data != form.password.data:
             raise ValidationError('The password needs to match the new '
@@ -152,7 +155,7 @@ class AccountForm(Form):
 	Validates current password entered with the password stored in database
 
 	:param field: The data value for the 'password' entered by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
         if form.user is not None:
             if not form.user.is_password_valid(field.data):
@@ -167,7 +170,7 @@ class AccountForm(Form):
 	Validates the new password entered
 
 	:param field: The data value for the 'password' entered by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
         if len(field.data) == 0 and \
                         len(form.new_password_repeat.data) == 0:
@@ -181,7 +184,7 @@ class AccountForm(Form):
 	Validates new password repeat and checks if it matches 'new_password'
 
 	:param field: The data value for the 'password' entered by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
         if form.email is not None:
             # Email form is present, so it's optional
@@ -220,7 +223,7 @@ class CompleteResetForm(Form):
 	Validates new password repeat and checks if it matches 'password'
 
 	:param field: The data value for the 'password' entered by User
-	:type field.data : str
+	:type field : PasswordField
 	"""
         if field.data != form.password.data:
             raise ValidationError('The password needs to match the new '

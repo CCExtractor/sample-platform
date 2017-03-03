bea003ab732291caa4c765aae27a72955445a353
Pranav Siddharth S <pranavsid98@gmail.com>
2017-03-03 22:03:08 +0530

    Documentation for mod_auth/forms.py (#57)

--- mod_auth/forms.py
+++ mod_auth/forms.py
@@ -7,13 +7,24 @@ from mod_auth.models import User, Role
 
 
 def unique_username(form, field):
-    # Check if a user already exists with this name
+    """
+	Check if a user already exists with this name
+
+	:param field: The data value for the 'name' inserted by new User
+	:type field : StringField
+	"""
     user = User.query.filter(User.name == field.data).first()
     if user is not None:
         raise ValidationError('There is already a user with this name')
 
 
 def valid_password(form, field):
+	"""
+	Function to check for validity of a password
+
+	:param field: The data value for the 'password' inserted by User
+	:type field : PasswordField
+	"""
     if len(field.data) == 0:
         raise ValidationError('new password cannot be empty')
     if len(field.data) < 10 or len(field.data) > 500:
@@ -23,7 +34,13 @@ def valid_password(form, field):
 
 
 def email_not_in_use(has_user_field=False):
-    def _email_not_in_use(form, field):
+	"""
+	Function to check if the passed email is already in use.
+
+	:param has_user_field : Whether an email has an existing User (False by default)
+	:type has_user_field : boolean
+	"""
+	def _email_not_in_use(form, field):
         user_id = -1 if not has_user_field else form.user.id
         # Check if email is not already in use
         user = User.query.filter(User.email == field.data).first()
@@ -33,12 +50,21 @@ def email_not_in_use(has_user_field=False):
 
 
 def role_id_is_valid(form, field):
+	"""
+	Checks for validity of User's Role
+
+	:param field : The data value for the 'role' inserted by User
+	:type field : SelectField
+	"""
     role = Role.query.filter(Role.id == field.data).first()
     if role is None:
         raise ValidationError('Role id is invalid')
 
 
 class LoginForm(Form):
+	"""
+	The form rendered when a User has to enter Log in credentials
+	"""
     email = EmailField('Email', [
         DataRequired(message='Email address is not filled in'),
         Email(message='Entered value is not a valid email address')
@@ -49,6 +75,9 @@ class LoginForm(Form):
 
 
 class SignupForm(Form):
+	"""
+	Sign up form for new Users.
+	"""
     email = EmailField('Email', [
         DataRequired(message='Email address is not filled in'),
         Email(message='Entered value is not a valid email address')
@@ -57,16 +86,25 @@ class SignupForm(Form):
 
 
 class DeactivationForm(Form):
+	"""
+	Deactivate existing account
+	"""
     submit = SubmitField('Deactivate account')
 
 
 class RoleChangeForm(Form):
+	"""
+	Changing the Role
+	"""
     role = SelectField('Select a role', [DataRequired(
         message='Role is not filled in.')], coerce=str)
     submit = SubmitField('Change role')
 
 
 class CompleteSignupForm(Form):
+	"""
+	The Complete Sign up form for new users.
+	"""
     name = StringField('Name', [DataRequired(
         message='Name is not filled in.')])
     password = PasswordField('Password', [DataRequired(
@@ -77,12 +115,21 @@ class CompleteSignupForm(Form):
 
     @staticmethod
     def validate_password_repeat(form, field):
+	"""
+	Validates if the repeated password is the same as 'password'
+
+	:param field : The data value for the 'password' entered by User
+	:type field : PasswordField
+	"""
         if field.data != form.password.data:
             raise ValidationError('The password needs to match the new '
                                   'password')
 
 
 class AccountForm(Form):
+	"""
+	Form for editing current Account
+	"""
     def __init__(self, formdata=None, obj=None, prefix='', *args, **kwargs):
         super(AccountForm, self).__init__(formdata, obj, prefix, *args,
                                           **kwargs)
@@ -104,6 +151,12 @@ class AccountForm(Form):
 
     @staticmethod
     def validate_current_password(form, field):
+	"""
+	Validates current password entered with the password stored in database
+
+	:param field: The data value for the 'password' entered by User
+	:type field : PasswordField
+	"""
         if form.user is not None:
             if not form.user.is_password_valid(field.data):
                 raise ValidationError('Invalid password')
@@ -113,6 +166,12 @@ class AccountForm(Form):
 
     @staticmethod
     def validate_new_password(form, field):
+	"""
+	Validates the new password entered
+
+	:param field: The data value for the 'password' entered by User
+	:type field : PasswordField
+	"""
         if len(field.data) == 0 and \
                         len(form.new_password_repeat.data) == 0:
             return
@@ -121,6 +180,12 @@ class AccountForm(Form):
 
     @staticmethod
     def validate_new_password_repeat(form, field):
+	"""
+	Validates new password repeat and checks if it matches 'new_password'
+
+	:param field: The data value for the 'password' entered by User
+	:type field : PasswordField
+	"""
         if form.email is not None:
             # Email form is present, so it's optional
             if len(field.data) == 0 and len(form.new_password.data) == 0:
@@ -132,6 +197,9 @@ class AccountForm(Form):
 
 
 class ResetForm(Form):
+	"""
+	Form for resetting password
+	"""
     email = EmailField('Email', [
         DataRequired(message='Email address is not filled in'),
         Email(message='Entered value is not a valid email address')
@@ -140,6 +208,9 @@ class ResetForm(Form):
 
 
 class CompleteResetForm(Form):
+	"""
+	Resetting password after clicking on the link in the email
+	"""
     password = PasswordField('Password', [DataRequired(
         message='Password is not filled in.'), valid_password])
     password_repeat = PasswordField('Repeat password', [DataRequired(
@@ -148,6 +219,12 @@ class CompleteResetForm(Form):
 
     @staticmethod
     def validate_password_repeat(form, field):
+	"""
+	Validates new password repeat and checks if it matches 'password'
+
+	:param field: The data value for the 'password' entered by User
+	:type field : PasswordField
+	"""
         if field.data != form.password.data:
             raise ValidationError('The password needs to match the new '
                                   'password')

98fb0358029054c02f626cf1a1bceb305d9e9213
Satyam <satyammittalid@gmail.com>
2017-06-18 23:19:02 +0530

    [FIX] added windows dual support (#76)
    Add subfolder system so that both VM's can run at the same time

--- mod_ci/controllers.py
+++ mod_ci/controllers.py
@@ -152,12 +152,13 @@ def kvm_processor(db, kvm_name, platform, repository, delay):
     with app.app_context():
         full_url = url_for('ci.progress_reporter', test_id=test.id,
                            token=test.token, _external=True, _scheme="https")
-    file_path = os.path.join(config.get('SAMPLE_REPOSITORY', ''), 'reportURL')
+    file_path = os.path.join(config.get('SAMPLE_REPOSITORY', ''),
+                             'vm_data', kvm_name, 'reportURL')
     with open(file_path, 'w') as f:
         f.write(full_url)
     # 1) Generate test files
     base_folder = os.path.join(
-        config.get('SAMPLE_REPOSITORY', ''), 'ci-tests')
+        config.get('SAMPLE_REPOSITORY', ''), 'vm_data', kvm_name, 'ci-tests')
     categories = Category.query.order_by(Category.id.desc()).all()
     commit_hash = GeneralData.query.filter(
         GeneralData.key == 'last_commit').first().value
@@ -229,7 +230,8 @@ def kvm_processor(db, kvm_name, platform, repository, delay):
     # 2) Create git repo clone and merge PR into it (if necessary)
     try:
         repo = Repo(os.path.join(
-            config.get('SAMPLE_REPOSITORY', ''), 'unsafe-ccextractor'))
+            config.get('SAMPLE_REPOSITORY', ''), 'vm_data', kvm_name,
+                    'unsafe-ccextractor'))
     except InvalidGitRepositoryError:
         log.critical('Could not open CCExtractor\'s repository copy!')
         return

c93353ed8b9f67aa54b94c435abea257021e6b82
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2017-07-15 17:23:33 +0200

    Update the process script

--- mod_upload/progress_ftp_upload.py
+++ mod_upload/progress_ftp_upload.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 import sys
 from os import path
-file_path = str(sys.argv[1])
 # Need to append server root path to ensure we can import the necessary files.
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from mod_upload.controllers import upload_ftp
-from database import create_session
-from run import config, log
-from mod_upload.models import QueuedSample
-from mod_sample.models import Sample
+
+
+def process(database, file_to_process):
+    from mod_upload.controllers import upload_ftp
+    from run import log
+    log.debug("Calling the FTP upload method from the controller!")
+    upload_ftp(database, file_to_process)
 
 if __name__ == '__main__':
+    from database import create_session
+    from run import config, log
+
     db = create_session(config['DATABASE_URI'])
-    log.info("In the progree ftp upload")
-    upload_ftp(db, file_path)
+    file_path = str(sys.argv[1])
+    process(db, file_path)

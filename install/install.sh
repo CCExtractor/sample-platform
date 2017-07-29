4a52bf6dba11dd3b4d45cdd24ecb1914514412cb
Satyam Mittal <satyammittalid@gmail.com>
2017-07-30 02:01:07 +0530

    added installation packages

--- install/install.sh
+++ install/install.sh
@@ -27,7 +27,7 @@ echo ""
 echo "* Updating package list        "
 apt-get update >> "$install_log" 2>&1
 echo "* Installing nginx, python, pip, kvm, libvirt and virt-manager"
-apt-get -q -y install nginx python python-dev libxslt1-dev libxml2-dev python-pip qemu-kvm libvirt-bin virt-manager >> "$install_log" 2>&1
+apt-get -q -y install nginx python python-dev libxslt1-dev libxml2-dev python-pip qemu-kvm libvirt-bin virt-manager mediainfo >> "$install_log" 2>&1
 if [ ! -f /etc/init.d/mysql* ]; then
     echo "* Installing MySQL (root password will be empty!)"
     DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server >> "$install_log" 2>&1
@@ -35,7 +35,7 @@ fi
 echo "* Update setuptools            "
 pip install --upgrade setuptools >> "$install_log" 2>&1
 echo "* Installing pip dependencies"
-pip install sqlalchemy flask passlib pymysql flask-wtf gunicorn githubpy requests pyIsEmail ipaddress GitPython xmltodict lxml pytz tzlocal >> "$install_log" 2>&1
+pip install sqlalchemy flask passlib pymysql python-magic flask-wtf gunicorn githubpy requests pyIsEmail ipaddress GitPython xmltodict lxml pytz tzlocal >> "$install_log" 2>&1
 echo ""
 echo "-------------------------------"
 echo "|        Configuration        |"

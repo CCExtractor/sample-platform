3d81e8345a60d9a4b57972d392554b55ea12c1f2
canihavesomecoffee <canihavesomecoffee@users.noreply.github.com>
2017-12-13 20:34:44 +0100

    Fix missing pr nr causing number to be set to 0
    Closes #119

--- mod_ci/controllers.py
+++ mod_ci/controllers.py
@@ -596,7 +596,7 @@ def start_ci():
             elif payload['action'] == 'reopened':
                 # Run tests again
                 queue_test(g.db, repository, gh_commit, commit,
-                           TestType.pull_request)
+                           TestType.pull_request, pr_nr=pr_nr)
         elif event == "issues":
             issue_data = payload['issue']
             issue = Issue.query.filter(

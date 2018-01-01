957367bca9a4d1e56cb3bc2c10b09830358c1c49
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-01-01 21:35:24 +0100

    Update models.py

--- mod_ci/models.py
+++ mod_ci/models.py
@@ -8,16 +8,6 @@ List of models corresponding to mysql tables: ['Kvm' => 'kvm',
 """
 
 import datetime
-from sqlalchemy import Column, Integer, String, ForeignKey, Da"""
-mod_ci Models
-===================
-In this module, we are trying to maintain all database related virtual
-machines and their status.
-List of models corresponding to mysql tables: ['Kvm' => 'kvm',
- 'Maintenance mode' => 'maintenance_mode']
-"""
-
-import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 
@@ -37,7 +27,6 @@ class Kvm(Base):
     def __init__(self, name, test_id, timestamp=None):
         """
         Parametrized constructor for the Kvm model
-
         :param name: The value of the 'name' field of Kvm model
         :type name: str
         :param test_id: The value of the 'test_id' field of Kvm model
@@ -56,7 +45,6 @@ class Kvm(Base):
         """
         Representation function
         Represent a Kvm Model by its 'test_id' Field.
-
         :return str(test_id): Returns the string containing
          'test_id' field of the Kvm model
         :rtype str(test_id): str
@@ -74,7 +62,6 @@ class MaintenanceMode(Base):
     def __init__(self, platform, mode):
         """
         Parametrized constructor for the MaintenanceMode model
-
         :param platform: The value of the 'platform' field of
          MaintenanceMode model
         :type platform: TestPlatform
@@ -88,7 +75,6 @@ class MaintenanceMode(Base):
         """
         Representation function
         Represent a MaintenanceMode Model by its platform and status Field.
-
         :return str(platform, status): Returns the string containing
          'platform' and 'status' field of the MaintenanceMode model
         :rtype str(platform, status): str

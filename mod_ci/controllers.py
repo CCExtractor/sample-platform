7e09f12d6ec5a0651cadb462d391771c476753c0
Aadi Bajpai <aadibajpai@gmail.com>
2018-01-03 02:29:22 +0530

    Indentation fix

--- mod_ci/controllers.py
+++ mod_ci/controllers.py
@@ -564,16 +564,16 @@ def start_ci():
                 except KeyError:
                     g.log.critical("Didn't find a SHA value for a newly opened PR!")
                     g.log.debug(payload)
-            # Check if user blacklisted
-            if BlockedUsers.query.filter(BlockedUsers.userID == payload[
-                   'pull_request']['user']['id']).first() is not None:
-                log.critical("User Blacklisted")
-                gh_commit.post(
-                    state=Status.ERROR,
-                    description="CI start aborted. You may be blocked from accesing this functionality",
-                    target_url=url_for('home.index', _external=True)
-                    )
-                return 'ERROR'
+                # Check if user blacklisted
+                if BlockedUsers.query.filter(BlockedUsers.userID == payload[
+                    'pull_request']['user']['id']).first() is not None:
+                    log.critical("User Blacklisted")
+                    gh_commit.post(
+                        state=Status.ERROR,
+                        description="CI start aborted. You may be blocked from accesing this functionality",
+                        target_url=url_for('home.index', _external=True)
+                        )
+                    return 'ERROR'
 
             elif payload['action'] == 'closed':
                 g.log.debug('PR was closed, no after hash available')

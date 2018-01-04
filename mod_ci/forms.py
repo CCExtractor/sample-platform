cc74633c6ae02d2152aed8031cb7242e0ea9d4b0
Aadi Bajpai <aadibajpai@gmail.com>
2018-01-05 02:47:58 +0530

    Added remove user form

--- mod_ci/forms.py
+++ mod_ci/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form
-from wtforms import IntegerField, StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import IntegerField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class AddUsersToBlacklist(Form):
-    userID = IntegerField('Add User', [DataRequired(
+    userID = IntegerField('User ID', [DataRequired(
         message='GitHub User ID not filled in')])
-    submit = SubmitField('Login')
+    submit = SubmitField('Add User')

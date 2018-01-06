bb829423e46630faee161a780529da3b6ac90e13
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-01-06 16:51:09 +0700

    Update controllers.py

--- mod_ci/controllers.py
+++ mod_ci/controllers.py
@@ -567,13 +567,13 @@ def start_ci():
                     g.log.debug(payload)
                 # Check if user blacklisted
                 if BlockedUsers.query.filter(BlockedUsers.userID == payload[
-                    'pull_request']['user']['id']).first() is not None:
+                        'pull_request']['user']['id']).first() is not None:
                     log.critical("User Blacklisted")
                     gh_commit.post(
                         state=Status.ERROR,
                         description="CI start aborted. You may be blocked from accessing this functionality",
                         target_url=url_for('home.index', _external=True)
-                        )
+                    )
                     return 'ERROR'
 
             elif payload['action'] == 'closed':
@@ -621,16 +621,18 @@ def start_ci():
         return json.dumps({'msg': 'EOL'})
 
 
-def check_status_for_badge(status,test):
+def check_status_for_badge(status, test):
     if test.test_type != test.pull_request:
-        dwg = svgwrite.Drawing('../static/svg/status-{platform}.svg'.format(platform=test.platform.value), profile='full')
+        dwg = svgwrite.Drawing(
+            '../static/svg/status-{platform}.svg'.format(platform=test.platform.value), profile='full')
         if status == Status.SUCCESS:
             dwg.add(dwg.text('Passing', insert=(0, 0.2), fill='green'))
-        elif status == Status.FAILURE: 
+        elif status == Status.FAILURE:
             dwg.add(dwg.text('Failing', insert=(0, 0.2), fill='red'))
         else:
             dwg.add(dwg.text('Unknown', insert=(0, 0.2), fill='gray'))
-        dwg.save() 
+        dwg.save()
+
 
 @mod_ci.route('/progress-reporter/<test_id>/<token>', methods=['POST'])
 def progress_reporter(test_id, token):
@@ -795,6 +797,7 @@ def progress_reporter(test_id, token):
                         state = Status.SUCCESS
                         message = 'Tests completed'
                         check_status_for_badge(state, test)
+
                 else:
                     message = progress.message
 

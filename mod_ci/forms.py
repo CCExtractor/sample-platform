3e8c6cc25114ece0ad54c0ffaec90ce4d71337f8
Aadi Bajpai <aadibajpai@gmail.com>
2018-01-21 20:49:54 +0530

    Add form fixes too

--- mod_ci/forms.py
+++ mod_ci/forms.py
@@ -6,9 +6,9 @@ from wtforms.validators import DataRequired
 class AddUsersToBlacklist(Form):
     user_id = IntegerField('User ID', [DataRequired(message='GitHub User ID not filled in')])
     comment = StringField('Comment')
-    submit = SubmitField('Add User')
+    add = SubmitField('Add User')
 
 
 class RemoveUsersFromBlacklist(Form):
     user_id = IntegerField('User ID', [DataRequired(message='GitHub User ID not filled in')])
-    submit = SubmitField('Remove User')
+    remove = SubmitField('Remove User')

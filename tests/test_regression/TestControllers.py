65a69fce6bc7fe93ddd7d549a820e292aad3363c
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-10-27 17:50:59 +0530

    Added unit test

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -30,3 +30,15 @@ class TestControllers(BaseTestCase):
                 self.assertEqual('False', response.json['active'])
             else:
                 self.assertEqual('True', response.json['active'])
+                
+    def test_regression_test_delete(self):
+        self.create_user_with_role(
+            self.user.name, self.user.email, self.user.password, Role.admin) # login with admin user
+        with self.app.test_client() as c:
+            response = c.get('/regression/test/15/delete')
+            self.assertEqual(response.status_code, 404) # test whether we are getting an error for deleting an invalid regression test
+            regression_test = RegressionTest.query.filter(RegressionTest.id == 1).first()
+            self.assertNotEqual(regression_test, None)
+            response = c.get('/regression/test/1/delete') # checks whether our function is Abel to delete present regression test
+            regression_test = RegressionTest.query.filter(RegressionTest.id == 1).first()
+            self.assertEqual(regression_test, None)                

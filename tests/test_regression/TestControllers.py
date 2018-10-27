b3ab52383f75f83c16369d1b6d05c6cac8ee1e98
matejmecka <matej.plavevski+github@gmail.com>
2018-10-27 13:34:51 +0200

    Update test to check if it'll throw 404

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -33,8 +33,21 @@ class TestControllers(BaseTestCase):
 
     def test_delete_if_will_abort_due_to_lack_of_permission(self):
         """
-        This will test if it will abort on an invalid test
+        This will test if it will abort on lack of permission
         :return:
         """
         response = self.app.test_client().get('/regression/test/9432/delete')
         self.assertEqual(response.status_code, 500)
+
+    def test_delete_if_will_throw_404(self):
+        """
+        Check if it will throw an error 404
+        :return:
+        """
+        self.create_user_with_role(
+            self.user.name, self.user.email, self.user.password, Role.admin)
+        with self.app.test_client() as c:
+            response = c.post(
+                '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
+            responseRegression = self.app.test_client().get('/regression/test/9432/delete')
+            self.assertEqual(responseRegression.status_code, 404)

6bef8d7e680bcc9117631a9f0f6ee0cc01187460
matejmecka <matej.plavevski+github@gmail.com>
2018-10-27 16:37:21 +0200

    Update 404 to 302

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -77,4 +77,4 @@ class TestControllers(BaseTestCase):
             response = c.post(
                 '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
             response_regression = c.get('/regression/test/1/delete')
-            self.assertEqual(response_regression.status_code, 404)
+            self.assertEqual(response_regression.status_code, 302) # 302 is code for redirection

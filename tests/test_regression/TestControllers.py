4e9dd62b4542f5ad6d5713113bef5ba1ba4adca9
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-10-27 22:27:13 +0530

    Update

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -30,18 +30,22 @@ class TestControllers(BaseTestCase):
                 self.assertEqual('False', response.json['active'])
             else:
                 self.assertEqual('True', response.json['active'])
-                
-    def test_regression_test_delete(self):
+
+    def test_regression_test_delete_invalid(self):
         self.create_user_with_role(
             self.user.name, self.user.email, self.user.password, Role.admin) # login with admin user
         with self.app.test_client() as c:
             response = c.post('/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
             response = c.get('/regression/test/1729/delete')
             self.assertEqual(response.status_code, 404) # test whether we are getting an error for deleting an invalid regression test
+
+    def test_regression_test_delete(self):
+        self.create_user_with_role(
+            self.user.name, self.user.email, self.user.password, Role.admin) # login with admin user
+        with self.app.test_client() as c:
+            response = c.post('/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
             regression_test = RegressionTest.query.filter(RegressionTest.id == 1).first()
             self.assertNotEqual(regression_test, None) # to test whether the regression test is present
             response = c.get('/regression/test/1/delete')
             regression_test = RegressionTest.query.filter(RegressionTest.id == 1).first()
-            self.assertEqual(regression_test, None) # checks whether our function is able to delete present regression test                
-                
-                
+            self.assertEqual(regression_test, None) # checks whether our function is able to delete present regression test

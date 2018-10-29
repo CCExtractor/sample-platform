3777b27cca4f8bc68a40403e063246c81fd68cad
T1duS <udit121sanghi@gmail.com>
2018-10-30 00:56:52 +0530

    Adding some files which I forgot

---
+++ mod_regression/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, ValidationError
+
+class AddCategoryForm(FlaskForm):
+    """
+    Flask form to Add Category
+    """
+    category_name = StringField('Category Name', [DataRequired(message="Category name can't be empty")])
+    category_description = StringField('Description')
+    submit = SubmitField("Add Category")

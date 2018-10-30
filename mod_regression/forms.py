8356af39213401a2106f430ea95c631ed45254fd
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-10-30 15:24:09 +0530

    Undo'ed forms.py

--- mod_regression/forms.py
+++ mod_regression/forms.py
@@ -6,6 +6,6 @@ class AddCategoryForm(FlaskForm):
     """
     Flask form to Add Category
     """
-    category_name = StringField('Category Name', [validators.InputRequired(message="Category name can't be empty")])
+    category_name = StringField('Category Name', [DataRequired(message="Category name can't be empty")])
     category_description = StringField('Description')
     submit = SubmitField("Add Category")

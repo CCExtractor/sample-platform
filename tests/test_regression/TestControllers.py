e7e1970f41af35bd37cfd0476ee0213181888814
T1duS <udit121sanghi@gmail.com>
2018-11-06 22:27:04 +0530

    Added a unit test

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -329,4 +329,32 @@ class TestControllers(BaseTestCase):
                     category_id = 2,
                     submit = True,
                 ))
-            self.assertEqual(response_regression.status_code, 404)            
+            self.assertEqual(response_regression.status_code, 404)
+
+    def test_edit_test_same_category(self):
+        """
+        Check it won't create problems edit a regression test and not changing its category
+        """
+        self.create_user_with_role(
+            self.user.name, self.user.email, self.user.password, Role.admin)
+        with self.app.test_client() as c:
+            response = c.post(
+                '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
+            response = c.post(
+                '/regression/test/1/edit', data=dict(
+                    sample_id = 1,
+                    command = "-demogorgans",
+                    input_type = "file",
+                    output_type = "file",
+                    category_id = 1,
+                    expected_rc = 25,
+                    submit = True,
+                ))
+            self.assertNotEqual(RegressionTest.query.filter(RegressionTest.command == "-demogorgans").first(),None)
+            category = Category.query.filter(Category.id == 1).first()
+            testisincategory = False
+            for i in category.regression_tests:
+                if i.id == 1:
+                    testisincategory = True
+                    break
+            self.assertEqual(testisincategory,True)                        

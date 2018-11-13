50293509be1ae837e2409a37a52f6c5b8cb03cfc
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-11-13 16:38:33 +0100

    Update tests

--- tests/test_regression/TestControllers.py
+++ tests/test_regression/TestControllers.py
@@ -371,8 +371,15 @@ class TestControllers(BaseTestCase):
 
     def test_if_test_toggle_view_throws_a_not_found_error(self):
         """
-        Check if the text doesn't exist and will throw an error 404
+        Check if the test toggle doesn't exist and will throw an error 404
         """
-        response = self.app.test_client().get('regression/test/1337/toggle') 
-        self.assertEqual(response.status_code, 404)
+        self.create_user_with_role(
+            self.user.name, self.user.email, self.user.password, Role.admin)
+
+        with self.app.test_client() as c:
+            response_login = c.post(
+                '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
+            
+            response = self.app.test_client().get('regression/test/1337/toggle') 
+            self.assertEqual(response.status_code, 404)
                 

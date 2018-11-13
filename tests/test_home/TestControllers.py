ad2c05700abe5cdf3584c668c8c94b35818d8e27
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-11-13 20:02:26 +0100

    Update TestControllers.py

--- tests/test_home/TestControllers.py
+++ tests/test_home/TestControllers.py
@@ -14,18 +14,18 @@ class TestControllers(BaseTestCase):
         self.assert_template_used('home/about.html')
 
     def test_if_user_has_test_access_rights(self):
-    	"""
-    	Test if the user will have rights to the tests
-    	"""
+        """
+        Test if the user will have rights to the tests
+        """
 
-    	self.create_user_with_role(
+        self.create_user_with_role(
             self.user.name, self.user.email, self.user.password, Role.admin)
 
-    	with self.app.test_client() as c:
-    		response_login = c.post(
+        with self.app.test_client() as c:
+            response_login = c.post(
                 '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))
 
-    		response = self.app.test_client().get('/')
-    		self.assertEqual(response.status_code, 200)
+            response = self.app.test_client().get('/')
+            self.assertEqual(response.status_code, 200)
             self.assertEqual(response.data['test_access'], 'True')
-    		self.assert_template_used('home/index.html')
+            self.assert_template_used('home/index.html')

f30468abef3c22362c60b51130ac37802a037817
Matej Plavevski <matej.plavevski+github@gmail.com>
2018-11-14 16:33:24 +0100

    Update TestControllers.py

--- tests/test_home/TestControllers.py
+++ tests/test_home/TestControllers.py
@@ -27,5 +27,5 @@ class TestControllers(BaseTestCase):
 
             response = self.app.test_client().get('/')
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data['test_access'], 'True')
+            self.assert_context('test_access', 'True')
             self.assert_template_used('home/index.html')

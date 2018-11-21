21cf66970806bd7ab9e19f1ae53d4df302c5a6f8
T1duS <udit121sanghi@gmail.com>
2018-11-21 18:07:09 +0530

    did the suggested changes

--- tests/test_auth/TestControllers.py
+++ tests/test_auth/TestControllers.py
@@ -185,7 +185,7 @@ class ManageAccount(BaseTestCase):
         Test editing account where github token is not null
         """
         self.create_user_with_role(
-            self.user.name, self.user.email, self.user.password, Role.admin, "GG")
+            self.user.name, self.user.email, self.user.password, Role.admin, self.user.github_token)
         with self.app.test_client() as c:
             response = c.post(
                 '/account/login', data=self.create_login_form_data(self.user.email, self.user.password))

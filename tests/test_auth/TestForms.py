89ca1e92f86cd1b9d4e44575d1e55199a413b81d
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-06-21 00:54:49 +0530

    add unit tests for mod_auth/forms.py

---
+++ tests/test_auth/TestForms.py
@@ -0,0 +1,63 @@
+from tests.base import BaseTestCase
+
+from mod_auth.forms import unique_username, valid_password
+
+from flask import g
+from mod_auth.models import User
+from wtforms.validators import ValidationError
+
+
+class Field:
+        def __init__(self, data):
+            self.data = data
+
+
+class TestForm(BaseTestCase):
+
+    def test_unique_username(self):
+        """
+        Test that username is always unique.
+        """
+        user = User(name="thealphadollar")
+        g.db.add(user)
+        g.db.commit()
+
+        user_field = Field("thealphadollar")
+
+        with self.assertRaises(ValidationError):
+            unique_username(None, user_field)
+
+    def test_empty_invalid_password(self):
+        """
+        Test validation fail for zero length password.
+        """
+        pass_field = Field("")
+
+        with self.assertRaises(ValidationError):
+            valid_password(None, pass_field)
+
+    def test_less_than_min_lenght_invalid_password(self):
+        """
+        Test validation fail for password of length less than min length.
+        """
+        pass_field = Field("".join(['x' * (int(self.app.config['MIN_PWD_LEN']) - 1)]))
+
+        with self.assertRaises(ValidationError):
+            valid_password(None, pass_field)
+
+    def test_more_than_max_lenght_invalid_password(self):
+        """
+        Test validation fail for password of length more than max length.
+        """
+        pass_field = Field("".join(['x' * (int(self.app.config['MAX_PWD_LEN']) + 1)]))
+
+        with self.assertRaises(ValidationError):
+            valid_password(None, pass_field)
+
+    def test_valid_password(self):
+        """
+        Test validation pass for valid password.
+        """
+        pass_field = Field("".join(['x' * (int(self.app.config['MAX_PWD_LEN']))]))
+
+        valid_password(None, pass_field)

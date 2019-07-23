95a00ff6a9d6aa225401b4460e26313d44bf8a6a
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2019-07-23 11:40:06 +0530

    Automate updation (#321)
    * add flask-script
    
    * Add manage.py
    
    - Manage.py is required so that we can handle operations that required the app context.
    - We can now call scripts from inside flask environment.
    - Makes the app more flexible to future development.
    
    * add methods to fetch information for ccextractor args
    
    * finalize update script
    
    * add dbsession instead of model query
    
    * add sample update instructions to readme
    
    * test and rectify bugs
    
    * Update manage.py
    
    remove redundant else statement.
    
    * Update manage.py
    
    simplification of if-else
    
    * Update manage.py
    
    * add tests
    
    * Update update_regression.py
    
    simply conditional statement.
    
    * ignore manage.py in test coverage

---
+++ mod_regression/update_regression.py
@@ -0,0 +1,138 @@
+"""Contain function for updating the outputs of regression test with latest CCExtractor version."""
+
+
+import os
+import subprocess
+from time import gmtime, strftime
+from typing import List
+
+from database import create_session
+from mod_regression.models import RegressionTest
+from run import config
+
+INPUT_VIDEO_FOLDER = os.path.join(config.get('SAMPLE_REPOSITORY', ''), 'TestFiles')
+OUTPUT_RESULTS_FOLDER = os.path.join(config.get('SAMPLE_REPOSITORY', ''), 'TestResults')
+LOG_DIR = os.path.join(os.path.expanduser('~'), 'sampleplatform_sample_update_logs')
+
+
+class Test:
+    """Object to hold all test details and get methods."""
+
+    def __init__(self, input_file: str, args: str, output: str) -> None:
+        self.input = input_file
+        self.args = args
+        self.output = output
+
+    @staticmethod
+    def get_inputfilepath(reg_test: RegressionTest) -> str:
+        """
+        Get absolute input video path from RegressionTest entry.
+
+        :param reg_test: regression test
+        :type reg_test: RegressionTest
+        :return: absolute file path of the input file
+        :rtype: str
+        """
+        file_name = reg_test.sample.filename
+        file_path = os.path.join(INPUT_VIDEO_FOLDER, file_name)
+        abs_file_path = os.path.abspath(file_path)
+
+        return abs_file_path
+
+    @staticmethod
+    def get_outputfilepath(reg_test: RegressionTest) -> str:
+        """
+        Get absolute output captions path from RegressionTest entry.
+
+        :param reg_test: regression test
+        :type reg_test: RegressionTest
+        :return: absolute file path of the output file
+        :rtype: str
+        """
+        output = reg_test.output_files[0]
+        file_name = output.filename_correct
+        file_path = os.path.join(OUTPUT_RESULTS_FOLDER, file_name)
+        abs_file_path = os.path.abspath(file_path)
+
+        return abs_file_path
+
+    @staticmethod
+    def run_ccex(path_to_ccex: str, log_file: str, input_file: str, args: str, output_file: str) -> bool:
+        """
+        Run ccextractor in a subprocess in a synchronous manner.
+
+        :param path_to_ccex: path to the latest ccextractor executable
+        :type path_to_ccex: str
+        :param log_file: absolute path to the log file where ccextractor output is saved
+        :type log_file: str
+        :param input_file: absolute path to the input file
+        :type input_file: str
+        :param args: arguments for ccextractor
+        :type args: str
+        :param output_file: absolute path to the output file
+        :type output_file: str
+        :return: True if successful, False otherwise
+        :rtype: bool
+        """
+        proc_args = [path_to_ccex]
+        proc_args.extend(list(args.split(' ')))
+        proc_args.append(input_file)
+        proc_args.extend(['-o', output_file])
+
+        try:
+            proc = subprocess.run(proc_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            with open(log_file, 'w+') as logf:
+                logf.write(proc.stdout.decode('utf-8'))
+            if proc.returncode != 0:
+                print(f'ERROR: ccextractor encountered error for {input_file}, please see {log_file}')
+                print(f'ccextractor was run as {" ".join(proc_args)}')
+                return False
+            return True
+        except subprocess.CalledProcessError as err:
+            print(f'ERROR: subprocess failed for {input_file}')
+            print(f'ERROR: The run for subprocess was "{" ".join(proc_args)}"')
+            return False
+
+
+def update_expected_results(path_to_ccex: str) -> bool:
+    """
+    Update expected result in the regression.
+
+    :param path_to_ccex: path to the ccextractor executable
+    :type path_to_ccex: str
+    """
+    DBSession = create_session(config['DATABASE_URI'])
+
+    if not os.path.isfile(path_to_ccex):
+        return False
+
+    all_regression_tests = DBSession.query(RegressionTest).all()
+
+    tests_to_update = []
+
+    if len(all_regression_tests) == 0:
+        print('INFO: No regression tests found!')
+        return True
+
+    for test in all_regression_tests:
+        input_file = Test.get_inputfilepath(test)
+        output_file = Test.get_outputfilepath(test)
+        args = test.command
+
+        tests_to_update.append(Test(
+            input_file,
+            args,
+            output_file
+        ))
+
+    log_folder_name = strftime("%Y_%m_%dT%H_%M_%SZ", gmtime())
+    log_folder_path = os.path.join(LOG_DIR, log_folder_name)
+    os.makedirs(log_folder_path, exist_ok=True)
+    print(f'INFO: ccextractor logs can be found at {log_folder_path} for each sample after update')
+
+    for test in tests_to_update:
+        log_file = os.path.join(log_folder_path, os.path.basename(test.input) + '.log')
+        success = Test.run_ccex(path_to_ccex, log_file, test.input, test.args, test.output)
+        if success:
+            print(f'SUCCESS: output {output_file} updated for sample {input_file}')
+    return True
---
+++ tests/test_regression/TestUpdateRegression.py
@@ -0,0 +1,189 @@
+import os
+from unittest import mock
+
+from tests.base import BaseTestCase
+
+
+class TestUpdateRegression(BaseTestCase):
+
+    def test_update_expected_results_not_path(self):
+        """
+        Test updating when ccextractor path is not correct.
+        """
+        from mod_regression.update_regression import update_expected_results
+
+        expected = False
+
+        response = update_expected_results('/not/a/valid/path')
+
+        self.assertEqual(response, expected)
+
+    @mock.patch('mod_regression.update_regression.os')
+    @mock.patch('mod_regression.update_regression.create_session')
+    def test_update_expected_results_zero_regressions(self, mock_session, mock_os):
+        """
+        Test updating when there are no regression tests.
+        """
+        from mod_regression.update_regression import update_expected_results
+
+        mock_os.path.isfile.return_value = True
+        mock_session.return_value.query.return_value.all.return_value = []
+        expected = True
+
+        response = update_expected_results('valid/path')
+
+        self.assertEqual(response, expected)
+
+    @mock.patch('mod_regression.update_regression.os')
+    @mock.patch('mod_regression.update_regression.Test')
+    def test_update_expected_results_(self, mock_test, mock_os):
+        """
+        Test updating regression tests.
+        """
+        from mod_regression.update_regression import update_expected_results
+
+        mock_os.path.isfile.return_value = True
+        expected = True
+        num_tests = 2   # store number of mock regression tests we have
+
+        response = update_expected_results('valid/path')
+
+        self.assertEqual(response, expected)
+        self.assertEqual(mock_test.get_inputfilepath.call_count, num_tests)
+        self.assertEqual(mock_test.get_outputfilepath.call_count, num_tests)
+        self.assertEqual(mock_test.call_count, num_tests)
+        mock_os.makedirs.assert_called_once()
+        self.assertEqual(mock_test.run_ccex.call_count, num_tests)
+
+    def test_Test_initiation(self):
+        """
+        Test initiation of Test class with mock arguments.
+        """
+        from mod_regression.update_regression import Test
+
+        filename = 'some.txt'
+        args = '--autotext'
+        output = 'someout.txt'
+
+        test = Test(
+            filename,
+            args,
+            output
+        )
+
+        self.assertEqual(test.input, filename)
+        self.assertEqual(test.args, args)
+        self.assertEqual(test.output, output)
+
+    def test_Test_get_inputfilepath(self):
+        """
+        Test method get_inputfilepath of Test class.
+        """
+        from mod_regression.update_regression import Test
+        from run import config
+
+        reg_test = mock.MagicMock()
+        reg_test.sample.filename = 'some.txt'
+        expected = os.path.abspath(os.path.join(os.path.join(config.get('SAMPLE_REPOSITORY', ''), 'TestFiles'),
+                                   reg_test.sample.filename))
+
+        filepath = Test.get_inputfilepath(reg_test)
+
+        self.assertEqual(filepath, expected)
+
+    def test_Test_get_outputfilepath(self):
+        """
+        Test method get_inputfilepath of Test class.
+        """
+        from mod_regression.update_regression import Test
+        from run import config
+
+        class MockOutput:
+            def __init__(self, filename):
+                self.filename_correct = filename
+
+        reg_test = mock.MagicMock()
+        reg_test.output_files = [MockOutput('output.txt')]
+        expected = os.path.abspath(os.path.join(os.path.join(config.get('SAMPLE_REPOSITORY', ''), 'TestResults'),
+                                   reg_test.output_files[0].filename_correct))
+
+        filepath = Test.get_outputfilepath(reg_test)
+
+        self.assertEqual(filepath, expected)
+
+    @mock.patch('mod_regression.update_regression.subprocess')
+    @mock.patch('mod_regression.update_regression.open')
+    def test_Test_run_ccex(self, mock_open, mock_subprocess):
+        """
+        Test run_ccex with proper arguments and no failure.
+        """
+        from mod_regression.update_regression import Test
+
+        mock_subprocess.run.return_value.returncode = 0
+        path_to_ccex = '/some/path'
+        log_file = 'some/log'
+        input_file = 'some/input'
+        args = '--autotext'
+        output_file = 'some/output'
+
+        result = Test.run_ccex(
+            path_to_ccex,
+            log_file,
+            input_file,
+            args,
+            output_file
+        )
+
+        self.assertEqual(result, True)
+
+    @mock.patch('mod_regression.update_regression.subprocess')
+    @mock.patch('mod_regression.update_regression.open')
+    def test_Test_run_ccex_with_error(self, mock_open, mock_subprocess):
+        """
+        Test run_ccex with proper arguments and failure of ccextractor.
+        """
+        from mod_regression.update_regression import Test
+
+        mock_subprocess.run.return_value.returncode = 1
+        path_to_ccex = '/some/path'
+        log_file = 'some/log'
+        input_file = 'some/input'
+        args = '--autotext'
+        output_file = 'some/output'
+
+        result = Test.run_ccex(
+            path_to_ccex,
+            log_file,
+            input_file,
+            args,
+            output_file
+        )
+
+        self.assertEqual(result, False)
+
+    @mock.patch('mod_regression.update_regression.subprocess')
+    @mock.patch('mod_regression.update_regression.open')
+    def test_Test_run_ccex_with_process_failure(self, mock_open, mock_subprocess):
+        """
+        Test run_ccex with proper arguments but failure of subprocess.
+        """
+        from mod_regression.update_regression import Test
+        from subprocess import CalledProcessError
+
+        mock_subprocess.run.side_effect = CalledProcessError(1, 'cmd')
+        mock_subprocess.CalledProcessError = CalledProcessError
+        path_to_ccex = '/some/path'
+        log_file = 'some/log'
+        input_file = 'some/input'
+        args = '--autotext'
+        output_file = 'some/output'
+
+        result = Test.run_ccex(
+            path_to_ccex,
+            log_file,
+            input_file,
+            args,
+            output_file
+        )
+
+        self.assertEqual(result, False)

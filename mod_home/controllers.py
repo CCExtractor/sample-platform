fed2bec6311763c7971ad4cabe922914616914bf
Willem <github@canihavesome.coffee>
2020-07-01 20:47:31 +0200

    Update controllers.py

--- mod_home/controllers.py
+++ mod_home/controllers.py
@@ -42,5 +42,5 @@ def about():
 
 @mod_home.route('/download_ccx')
 def download_ccx():
-    path_to_tester = path.join(app.config['SAMPLE_REPOSITORY'], 'TestSuite', 'CCExtractorTester.exe')
+    path_to_tester = '/repository/TestSuite/CCExtractorTester.exe')
     return send_file(path_to_tester, as_attachment=True, attachment_filename='ccextractortester.exe')

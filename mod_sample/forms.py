ba1715017a8de58c1738e3f8ca1b57dc3f59bfd5
Venkata Shravan <kandadishravan@gmail.com>
2021-06-03 10:48:14 +0530

    Edit sample form

--- mod_sample/forms.py
+++ mod_sample/forms.py
@@ -1,14 +1,24 @@
 """Maintains forms related to sample CRUD operations."""
 
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SelectField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError
 
-from mod_upload.forms import CommonSampleForm
+from mod_upload.models import Platform
 
 
-class EditSampleForm(CommonSampleForm):
+class EditSampleForm(FlaskForm):
     """Form to edit sample."""
 
+    notes = TextAreaField('Notes', [DataRequired(message='Notes are not filled in')])
+    parameters = TextAreaField('Parameters', [DataRequired(message='Parameters are not filled in')])
+    platform = SelectField(
+        'Platform',
+        [DataRequired(message='Platform is not selected')],
+        coerce=str,
+        choices=[(p.value, p.description) for p in Platform]
+    )
+    version = SelectField('Version', [DataRequired(message='Version is not selected')], coerce=int)
     submit = SubmitField('Update sample')
 
 

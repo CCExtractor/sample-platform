450814e3d7b1a064afba839d45425fb6afe7add4
Tarun-Arora <tarun.arora.cse20@iitbhu.ac.in>
2022-01-08 20:01:06 +0530

    Made Requested Changes

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,24 +1,23 @@
-const lightThemeCSS = "/static/css/foundation-light.min.css";
-const darkThemeCSS = "/static/css/foundation-dark.min.css";
-var toDark, toLight;
-
 $(document).ready(() => {
-    toDark = document.querySelector('.to-dark');
-    toLight = document.querySelector('.to-light');
-
-    document.querySelectorAll('.theme')
-        .forEach( button => button.addEventListener('click', toggleTheme) )
+    $('.theme-toggle i.theme').on('click', toggleTheme);
+    const toLight = document.querySelector('.to-light');
 
     // Fetch default theme from localStorage and apply it.
-    var theme = localStorage.getItem("data-theme");
+    const theme = localStorage.getItem("data-theme");
     if (theme === 'dark') toggleTheme();
     else toLight.style.display = "none";
 });
 
 
 function toggleTheme() {
-    document.documentElement.toggleAttribute("dark")
+    document.documentElement.toggleAttribute("dark");
     const theme = document.querySelector("#theme-link");
+    const toDark = document.querySelector('.to-dark');
+    const toLight = document.querySelector('.to-light');
+    
+    const lightThemeCSS = "/static/css/foundation-light.min.css";
+    const darkThemeCSS = "/static/css/foundation-dark.min.css";
+
     if (theme.getAttribute("href") == lightThemeCSS) {
         theme.href = darkThemeCSS;
         toDark.style.display = "none";

959c0781b6ebdf3c3c66ab54056a0df65713caf3
Tarun Arora <78356489+Tarun-Arora@users.noreply.github.com>
2022-01-09 21:50:35 +0530

    Fixed Theme Upgrade Issues (#621)

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,33 +1,35 @@
 $(document).ready(() => {
     $('.theme-toggle i.theme').on('click', toggleTheme);
-    const toLight = document.querySelector('.to-light');
+    // Toggle Icon Not Shown Until Page Completely Loads
 
     // Fetch default theme from localStorage and apply it.
     const theme = localStorage.getItem("data-theme");
     if (theme === 'dark') toggleTheme();
-    else toLight.style.display = "none";
+    else $('.theme-toggle i.to-dark').removeClass('hidden');
 });
 
 
 function toggleTheme() {
     document.documentElement.toggleAttribute("dark");
     const theme = document.querySelector("#theme-link");
-    const toDark = document.querySelector('.to-dark');
-    const toLight = document.querySelector('.to-light');
+    
+    const toDark = $('.theme-toggle i.to-dark');
+    const toLight = $('.theme-toggle i.to-light');
+    // Not Allowing User to Toggle Theme Again Before This Gets Completed. 
+    toDark.addClass('hidden')   
+    toLight.addClass('hidden')
     
     const lightThemeCSS = "/static/css/foundation-light.min.css";
     const darkThemeCSS = "/static/css/foundation-dark.min.css";
 
     if (theme.getAttribute("href") == lightThemeCSS) {
         theme.href = darkThemeCSS;
-        toDark.style.display = "none";
-        toLight.style.display = '';
         localStorage.setItem("data-theme", "dark");
+        toLight.removeClass('hidden')
     }
     else {
         theme.href = lightThemeCSS;
-        toDark.style.display = '';
-        toLight.style.display = "none";
         localStorage.setItem("data-theme", "light");
+        toDark.removeClass('hidden')
     }
 }

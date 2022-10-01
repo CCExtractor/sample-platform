febb1351966dbd0c05f403a63afc350f10fb05e4
Tarun Arora <78356489+Tarun-Arora@users.noreply.github.com>
2022-10-01 16:39:14 +0530

    [FIX] Fixes for tests on GCP VM instances (#702)
    * Fixes for Linux tests
    
    * Fixes for Windows Tests
    
    * Update preparation POST message in runCI files

--- install/ci-vm/ci-linux/startup-script.sh
+++ install/ci-vm/ci-linux/startup-script.sh
@@ -8,13 +8,13 @@ apt install gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-apt install mono-complete -y
+apt install -y mono-complete libtesseract-dev
 
 mkdir repository
 cd repository
 
 # Use gcsfuse and import required files
-mkdir temp TestFiles TestResults
+mkdir temp TestFiles TestResults vm_data reports
 gcsfuse --only-dir=TestData/ci-linux/ spdev temp
 cp temp/* ./
 umount temp
@@ -22,11 +22,14 @@ rmdir temp
 
 vm_name=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google")
 vm_name=(${vm_name//./ })
-mkdir vm_data reports
 
-sudo gcsfuse --implicit-dirs --only-dir vm_data/${vm_name} spdev vm_data
-sudo gcsfuse --implicit-dirs --only-dir TestFiles spdev TestFiles
-sudo gcsfuse --implicit-dirs --only-dir TestResults spdev TestResults
+echo "spdev   /repository/vm_data         gcsfuse rw,uid=33,gid=33,noatime,async,_netdev,noexec,user,implicit_dirs,allow_other,only_dir=vm_data/${vm_name}  0 0" | sudo tee -a /etc/fstab
+echo "spdev   /repository/TestFiles     gcsfuse rw,uid=33,gid=33,noatime,async,_netdev,noexec,user,implicit_dirs,allow_other,only_dir=TestFiles  0 0" | sudo tee -a /etc/fstab
+echo "spdev   /repository/TestResults     gcsfuse rw,uid=33,gid=33,noatime,async,_netdev,noexec,user,implicit_dirs,allow_other,only_dir=TestResults  0 0" | sudo tee -a /etc/fstab
+
+mount vm_data
+mount TestFiles
+mount TestResults
 
 chmod +x bootstrap
 ./bootstrap

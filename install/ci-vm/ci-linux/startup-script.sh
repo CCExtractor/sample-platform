540c4fac9f2175eb32c5fa6d7ecb80fc8d44b0b2
Willem <github@canihavesome.coffee>
2023-05-31 17:41:05 +0000

    Update startup-script.sh

--- install/ci-vm/ci-linux/startup-script.sh
+++ install/ci-vm/ci-linux/startup-script.sh
@@ -8,7 +8,7 @@ apt install gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-apt install -y mono-complete libtesseract-dev
+apt install -y mono-complete libtesseract-dev libgpac-dev
 
 mkdir repository
 cd repository

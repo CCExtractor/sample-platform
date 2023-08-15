1f0fdb3539e43a2b3a1a5d13673ca004f3901949
Tarun Arora <78356489+Tarun-Arora@users.noreply.github.com>
2023-08-15 20:09:37 +0530

    [FIX] Change boolean type from bit to tinyint (#847)

---
+++ migrations/versions/a5183973c3e9_.py
@@ -0,0 +1,23 @@
+"""Change SQLAlchemy Boolean bit(1) to bool
+
+Revision ID: a5183973c3e9
+Revises: 2e0d2e02a721
+Create Date: 2023-08-10 15:51:13.537000
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'a5183973c3e9'
+down_revision = '2e0d2e02a721'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('regression_test', 'active', existing_type=sa.dialects.mysql.BIT(1), type_=sa.Boolean(), nullable=False)
+
+
+def downgrade():
+    op.alter_column('regression_test', 'active', existing_type=sa.Boolean(), type_=sa.dialects.mysql.BIT(1), nullable=False)

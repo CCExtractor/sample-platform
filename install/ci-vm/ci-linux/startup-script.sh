65bbc3633785941a24f2fe8035bb0a884529a565
Hridesh MG <hridesh699@gmail.com>
2025-07-19 14:13:53 +0530

    fix: add missing font

--- install/ci-vm/ci-linux/startup-script.sh
+++ install/ci-vm/ci-linux/startup-script.sh
@@ -8,7 +8,7 @@ apt install gnupg ca-certificates
 gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-apt install -y mono-complete libtesseract-dev libgpac-dev tesseract-ocr-eng
+apt install -y mono-complete libtesseract-dev libgpac-dev tesseract-ocr-eng fonts-noto-core
 
 mkdir repository
 cd repository

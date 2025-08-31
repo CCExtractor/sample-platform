9ea65828d0087f2e7806e5b4c913fce54cce216c
Hridesh MG <hridesh699@gmail.com>
2025-08-31 21:29:06 +0530

    fix: tesseract not installed on Windows

--- install/ci-vm/ci-windows/startup-script.ps1
+++ install/ci-vm/ci-windows/startup-script.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install winfsp -y
+choco install winfsp tesseract -y
 
 cd C:\Windows\Temp
 
